45bcf68030995e5c94f6b0a3bdb00f22174002ec
Joe <josephnewton861@yahoo.co.uk>
2020-07-21 20:06:20 +0100

    Added first object type and root query ready for db seeding

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,1 +1,25 @@
 const graphql = require("graphql");
+
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const EventType = new GraphQLObjectType({
+  name: "Event",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    category: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    event: {
+      type: EventType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code to get data from db or other source
+      },
+    },
+  },
+});
