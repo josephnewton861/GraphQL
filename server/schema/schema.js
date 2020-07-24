ef3a79c5261d073302259a9ea024afb82b899ceb
Joe <josephnewton861@yahoo.co.uk>
2020-07-24 11:24:07 +0100

    Added promoters query so users can query by events by promoter or vice versa

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,47 +1,87 @@
 const graphql = require("graphql");
 const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLList,
+} = graphql;
 
 const events = [
   {
     name: "Glastonbury",
     category: "Music",
     id: "1",
+    promoterId: "1",
   },
   {
     name: "Champions League",
     category: "Sport",
     id: "2",
+    promoterId: "2",
   },
   {
     name: "Russel Howard",
     category: "Entertainment",
     id: "3",
+    promoterId: "3",
   },
   {
     name: "Superbowl",
     category: "Sport",
     id: "4",
+    promoterId: "2",
   },
   {
     name: "Bastille",
     category: "Music",
     id: "5",
+    promoterId: "1",
   },
   {
     name: "Shrek's Adventure",
     category: "Entertainment",
     id: "6",
+    promoterId: "3",
   },
+];
+
+const promoters = [
+  { name: "Acapella Events", ceo: "John Waterman", id: "1" },
+  { name: "Everything sports", ceo: "Bud Holdness", id: "2" },
+  { name: "Dream Events", ceo: "Susan Barnes", id: "3" },
 ];
 
 const EventType = new GraphQLObjectType({
   name: "Event",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     category: { type: GraphQLString },
+    promoter: {
+      type: PromoterType,
+      resolve(parent, args) {
+        return _.find(promoters, { id: parent.promoterId });
+      },
+    },
+  }),
+});
+
+const PromoterType = new GraphQLObjectType({
+  name: "Promoter",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    ceo: { type: GraphQLString },
+    events: {
+      type: new GraphQLList(EventType),
+      resolve(parent, args) {
+        // console.log(parent);
+        return _.filter(events, { promoterId: parent.id });
+      },
+    },
   }),
 });
 
@@ -50,10 +90,17 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     event: {
       type: EventType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data from db or other source
         return _.find(events, { id: args.id });
+      },
+    },
+    promoter: {
+      type: PromoterType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(promoters, { id: args.id });
       },
     },
   },
